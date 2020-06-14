2af2e8dd68e2c02b296820a06e029f7f6aa26b91
TNORTHERN <traynorthern@yahoo.com>
2020-06-14 12:44:41 -0500

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import { ApolloProvider } from '@apollo/react-hooks'
 import { Provider } from 'react-redux'
-import { useStore } from '../lib/redux'
-import { useApollo } from '../lib/apollo'
+import { useStore } from '../src/lib/redux'
+import { useApollo } from '../src/lib/apollo'
+import '../src/styles/tailwind.css'
 
 export default function App({ Component, pageProps }) {
   const store = useStore(pageProps.initialReduxState)
--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,10 @@
-import { useDispatch } from 'react-redux'
-import { initializeStore } from '../lib/redux'
-import { initializeApollo } from '../lib/apollo'
-import useInterval from '../lib/useInterval'
-import Layout from '../components/Layout'
-import Clock from '../components/Clock'
-import Counter from '../components/Counter'
-import Submit from '../components/Submit'
-import PostList, {
-  ALL_POSTS_QUERY,
-  allPostsQueryVars,
-} from '../components/PostList'
-
-const IndexPage = () => {
-  // Tick the time every second
-  const dispatch = useDispatch()
-
-  useInterval(() => {
-    dispatch({
-      type: 'TICK',
-      light: true,
-      lastUpdate: Date.now(),
-    })
-  }, 1000)
-
+const index = () => {
   return (
-    <Layout>
-      {/* Redux */}
-      <Clock />
-      <Counter />
-      <hr />
-      {/* Apollo */}
-      <Submit />
-      <PostList />
-    </Layout>
-  )
+    <div className="p-4 shadow rounded bg-white">
+      <h1 className="text-purple-500 leading-normal">Next.js</h1>
+      <p className="text-gray-500">with Tailwind CSS</p>
+    </div>
+  );
 }
 
-export async function getStaticProps() {
-  const reduxStore = initializeStore()
-  const apolloClient = initializeApollo()
-  const { dispatch } = reduxStore
-
-  dispatch({
-    type: 'TICK',
-    light: true,
-    lastUpdate: Date.now(),
-  })
-
-  await apolloClient.query({
-    query: ALL_POSTS_QUERY,
-    variables: allPostsQueryVars,
-  })
-
-  return {
-    props: {
-      initialReduxState: reduxStore.getState(),
-      initialApolloState: apolloClient.cache.extract(),
-    },
-    unstable_revalidate: 1,
-  }
-}
-
-export default IndexPage
+export default index;
